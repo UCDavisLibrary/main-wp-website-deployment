c27147bec7ff4d8ce98aafa4d7ba378c2c5e8c9b
Justin Merz <jrmerz@gmail.com>
2022-08-03 10:06:20 -0700

    quick fix for monitoring script

--- utils/monitoring/monitor.sh
+++ utils/monitoring/monitor.sh
@@ -2,7 +2,7 @@
 
 # start the monitoring agent
 if [[ $ENABLE_GC_APACHE_MONITORING == 'true' ]]; then
-  node /cmds/apache.js 
+  node /util-cmds/apache.js 
 fi
 
 echo "Exiting ENABLE_GC_APACHE_MONITORING not set to true"
