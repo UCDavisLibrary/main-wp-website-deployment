e1b39eff2ff00ebb18be364fbf57cd838e79ffc9
Quinn Hart <qjhart@ucdavis.edu>
2022-09-27 16:38:49 -0700

    v3.1.5

--- config.sh
+++ config.sh
@@ -18,11 +18,11 @@ APP_VERSION=v3.1.4.${BUILD_NUM}
 # Repository tags/branchs
 # Tags should always be used for production deployments
 # Branches can be used for development deployments
-WEBSITE_TAG=v3.1.4
+WEBSITE_TAG=v3.1.5
 
 # Submodules
 # only used for init-local-dev checkout
-WP_PLUGINS_SUB_TAG=v3.1.4
+WP_PLUGINS_SUB_TAG=v3.1.5
 WP_THEME_SUB_TAG=v3.1.4
 
 CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
