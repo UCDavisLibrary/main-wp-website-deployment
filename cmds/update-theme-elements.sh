31842f560c747a752da66f19ed3efaacc23c9d0b
spelkey-ucd <spelkey@ucdavis.edu>
2022-11-30 13:25:50 -0500

    add script to update all theme elements packages

---
+++ cmds/update-theme-elements.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+###
+# Updates theme elements npm package to latest
+###
+
+set -e
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $ROOT_DIR/..
+
+source ./config.sh
+
+for package in "${NPM_PRIVATE_PACKAGES[@]}"; do
+  cd $package;
+  if $NPM list | grep -q "@ucd-lib/theme-elements"; then
+    $NPM i @ucd-lib/theme-elements@latest --save
+  fi
+  cd $ROOT_DIR/..
+done
