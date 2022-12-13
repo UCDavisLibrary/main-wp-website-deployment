85fd38cd49735d309a48aec1f65ab4fb0b02ecd4
spelkey-ucd <spelkey@ucdavis.edu>
2022-12-13 15:35:18 -0500

    v3.2.0

--- config.sh
+++ config.sh
@@ -18,12 +18,12 @@ APP_VERSION=v3.2.0.${BUILD_NUM}
 # Repository tags/branchs
 # Tags should always be used for production deployments
 # Branches can be used for development deployments
-WEBSITE_TAG=stage
+WEBSITE_TAG=v3.2.0
 
 # Submodules
 # only used for init-local-dev checkout
-WP_PLUGINS_SUB_TAG=stage
-WP_THEME_SUB_TAG=stage
+WP_PLUGINS_SUB_TAG=v3.2.0
+WP_THEME_SUB_TAG=v3.1.5
 
 CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [[ -f "$CONFIG_DIR/main-website-content-reader-key.json" ]]; then
