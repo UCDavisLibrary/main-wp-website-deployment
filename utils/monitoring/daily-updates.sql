f0faf93bc99396dd1e75a247c5452f10373358ec
Quinn Hart <qjhart@ucdavis.edu>
2023-02-24 11:08:03 -0800

    search on posts not revisions
    Update for more coverage

--- utils/monitoring/daily-updates.sql
+++ utils/monitoring/daily-updates.sql
@@ -1,19 +1,20 @@
-select 
+select
   count(*) as update_count,
-  max(revision.post_modified) as last_updated,
-  posts.post_name as post_name,
-  posts.post_type as type,
-  posts.ID as post_id,
+  post.post_modified as last_updated,
+  post.post_name as post_name,
+  post.post_type as type,
+  post.ID as post_id,
   ruser.user_email as updated_by,
   puser.user_email as created_by
-from 
-   wp_posts revision
-left join wp_posts posts on revision.post_parent = posts.ID
+from
+   wp_posts post
+left join wp_posts revision on revision.post_parent = post.ID
 left join wp_users ruser on ruser.ID = revision.post_author
-left join wp_users puser on puser.ID = posts.post_author
+left join wp_users puser on puser.ID = post.post_author
 WHERE
-   revision.post_modified > date_sub(now(), INTERVAL ? DAY) and 
-   revision.post_type = 'revision' and
-   posts.post_status = 'publish'
+   post.post_status = 'publish' and
+   post.post_parent='' and
+   post.post_modified > date_sub(now(), INTERVAL ? DAY)
 group by
-   posts.post_name, posts.post_type, posts.Id, ruser.user_email, puser.user_email; 
+   post.post_name, post.post_type, post.ID, ruser.user_email, puser.user_email
+order by post_name,updated_by
