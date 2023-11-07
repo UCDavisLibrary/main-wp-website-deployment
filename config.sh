b189f6eec88c3fb6ea5c13640f518e449bc2e863
spelkey-ucd <spelkey@ucdavis.edu>
2023-11-07 16:11:06 -0500

    v3.8.1

--- config.sh
+++ config.sh
@@ -14,17 +14,17 @@ fi
 # Main version number we are tagging the app with. Always update
 # this when you cut a new version of the app!
 APP_VERSION=v3.8.1.${BUILD_NUM}
-APP_TAG=sandbox
+APP_TAG=stage
 
 # Repository tags/branchs
 # Tags should always be used for production deployments
 # Branches can be used for development deployments
-WEBSITE_TAG=sandbox
+WEBSITE_TAG=stage
 
 # Submodules
 # only used for init-local-dev checkout
-WP_PLUGINS_SUB_TAG=sandbox
-WP_THEME_SUB_TAG=sandbox
+WP_PLUGINS_SUB_TAG=stage
+WP_THEME_SUB_TAG=stage
 
 CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [[ -f "$CONFIG_DIR/main-website-content-reader-key.json" ]]; then
