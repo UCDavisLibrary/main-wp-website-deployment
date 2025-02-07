fb3d8a37da3367dc5c9bfd41ba6a051346562424
Justin Merz <jrmerz@gmail.com>
2025-02-07 15:47:34 -0800

    repo cleanup

--- cmds/init-keys.sh
+++ cmds/init-keys.sh
@@ -8,4 +8,4 @@ set -e
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $ROOT_DIR/..
 
-gcloud secrets versions access latest --secret=main-website-content-reader-key > main-website-content-reader-key.json
+gcloud secrets versions access latest --secret=main-website-content-reader-key > ./secrets/main-website-content-reader-key.json
